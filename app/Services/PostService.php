761df5a9d580ad789d899806e15b3174110e7759
namnt0413 <namnt0413@gmail.com>
2023-06-18 00:52:35 +0700

    fix filter posts with options

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -15,8 +15,31 @@ class PostService
 
     public function filterPosts($request)
     {
-        $filterPosts = Post::select('*')->where([ 'status' => 2 ])
-        ->where('title', 'LIKE', '%' . $request->title . '%')->with('user:id,name,logo')->get();
+        // $filterPosts = Post::select('*')->where([ 'status' => 2 ])
+        // ->where('title', 'LIKE', '%' . $request->title . '%')->with('user:id,name,logo')->get();
+
+        $queryRaw = "";
+        if(sizeof($request->options) !== 0) {
+            foreach($request->options as $option)
+            {
+                $queryRaw == '' ? $queryRaw = $queryRaw . 'user_options.user_id IN ( SELECT user_options.user_id from user_options WHERE user_options.option_id = '.$option.' ) '
+                            : $queryRaw = $queryRaw . 'and user_options.user_id IN ( SELECT user_options.user_id from user_options WHERE user_options.option_id = '.$option.' ) ';
+            }
+            $filterPosts = Post::selectRaw('DISTINCT posts.id,posts.title,posts.content,posts.image,posts.user_id,posts.created_at',)
+            ->join('users','posts.user_id','=','users.id')
+            ->join('user_options','users.id','=','user_options.user_id')
+            ->where([ 'posts.status' => 2 ])
+            ->where('posts.title', 'LIKE', '%' . $request->title . '%')
+            ->whereRaw($queryRaw)
+            ->with('user:id,name,logo')->get();
+        }  else {
+            $filterPosts = Post::selectRaw('DISTINCT posts.id,posts.title,posts.content,posts.image,posts.user_id,posts.created_at',)
+            ->join('users','posts.user_id','=','users.id')
+            ->join('user_options','users.id','=','user_options.user_id')
+            ->where([ 'status' => 2 ])
+            ->where('posts.title', 'LIKE', '%' . $request->title . '%')
+            ->with('user:id,name,logo')->get();
+        }
         return $filterPosts;
     }
 
